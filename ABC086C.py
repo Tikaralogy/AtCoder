d0b0a117f5da389b17d07c3ca5a60bc2218529bb
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2021-11-14 18:59:14 +0900

    11/14

--- ABC086C.py
+++ ABC086C.py
@@ -5,14 +5,13 @@ for i in range(n):
 ls.insert(0, [0,0,0])
 z = 0
 for i in range(n-1):
-    t = abs(ls[i+1][0]-ls[i][0]) #時間差
+    t = ls[i+1][0]-ls[i][0] #時間差
     x = abs(ls[i+1][1]-ls[i][1]) #x差
     y = abs(ls[i+1][2]-ls[i][2]) #y差
     if (t - x - y)%2==0 and (t - x - y)>0:
         z += 0
     else:
         z += 1
-print(z)
 if z == 0:
     print("Yes")
 else:
