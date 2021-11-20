6e37fbc4c8c51f6eb4412bde9f1f9b17aeb9d8a3
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2021-11-20 15:17:52 +0900

    2021/11/20

--- ABC133B.py
+++ ABC133B.py
@@ -7,8 +7,8 @@ dis = []
 total = 0
 for a in range(n):
     for b in range(a+1,n):
-        dis = [(a[c]-b[c])**2 for c in range(d)]
+        dis = [(ls[a][c]-ls[b][c])**2 for c in range(d)]
         if pow(sum(dis),0.5) == int(pow(sum(dis),0.5)):
             total += 1
-print(dis)
+print(total)
 
