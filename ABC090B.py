83ce5ff2154596e5c0ceb749b694dd62fc7f099b
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2021-11-22 09:38:09 +0900

    2021/11/22

--- ABC090B.py
+++ ABC090B.py
@@ -1,18 +1,18 @@
 a,b = map(int, input().split())
-x = []
 total = 0
 s = 0
+x = []
 for i in range(a,b+1):
     while True:
         if i == 0:
             break
         x.append(i%10)
         i = i//10
-    for j in range((len(x)-1)//2):
+    for j in range(len(x)//2):
         if x[j]==x[len(x)-j-1]:
             s +=1
-    if s == ((len(x)-1)//2+1):
+    if s == (len(x)//2):
         total += 1
-    s == 0
+    s = 0
     x = []
 print(total)
