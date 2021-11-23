cf8286c7d894636609b40e8f1142b13f5264a1f4
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2021-11-24 00:38:00 +0900

    2021/11/24

--- AGC027A.py
+++ AGC027A.py
@@ -2,14 +2,13 @@ n,x = map(int, input().split())
 ls = list(map(int, input().split()))
 ls.sort()
 t = 0
-print(ls)
-while True:
-    if x == 0 and ls == []:
-        break
-    if ls==[] and x >0:
-        t -=1
-    x -= ls[0]
-    t +=1
-    ls = ls[1:]
-print(t)
-
+s = 0
+if sum(ls)==x:
+    print(n)
+else:
+    for i in range(n):
+        if t > x:
+            break
+        t += ls[i]
+        s += 1
+    print(s-1)
