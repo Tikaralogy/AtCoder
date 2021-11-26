a5bebc2f23923887a19238a5ab161f3ac124cd73
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2021-11-27 05:15:59 +0900

    2021/11/27

---
+++ ABC081C.py
@@ -0,0 +1,15 @@
+n,k = map(int, input().split())
+ls = list(map(int, input().split()))
+dic = {}
+x = 0
+for value in ls:
+    dic[value]=ls.count(value)
+ls1 = list(dic.values())
+ls1.sort()
+if len(dic)<=k:
+    print(0)
+else:
+    while len(ls1)>k:
+        x += ls1[0]
+        ls1 = ls1[1:]
+    print(x)
