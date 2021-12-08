9d5e00e1f98de9f34f27fea9cc4f1e86090f2708
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2021-12-08 16:11:55 +0900

    2021/12/8

--- 練習/ARC096C.py
+++ 練習/ARC096C.py
@@ -1,6 +1,20 @@
 a,b,c,x,y=map(int,input().split())
 s = a*x + b*y
-if 2*c < a + b:
+if 2*c < a:
+    if x > y:
+        s = 2*c*x
+    elif 2*c < b:
+        s = 2*c*max(x,y)
+    else:
+        s = 2*c*x + (y-x)*b
+elif 2*c < b:
+    if x > y:
+        s = 2*c*y + (x-y)*a
+    elif 2*c < a:
+        s = 2*c*max(x,y)
+    else:
+        s = 2*c*x
+elif 2*c < a + b:
     s -= min(x,y)*(a+b)
     s += min(x,y)*2*c
     if x > y:
