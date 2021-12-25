cdca202cf51ef7774e1f891406939889d3c002cf
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2021-12-25 22:57:43 +0900

    2021/12/25

--- ABC232/test.py
+++ ABC232/test.py
@@ -1,15 +1,3 @@
-n,m = map(int,input().split())
-lsab = [] 
-lsc = [] 
-for i in range(m): 
-	lsab.append(list(map(int,input().split())))
-for i in range(m): 
-	lsc.append(list(map(int,input().split())))
-print(lsab)
-print(lsc)
-l = [3,2,1,4]
-a = lsab[1]
-print(a)
-print([l[a[0]-1],l[a[1]-1]]) 
-print([l[a[1]-1],l[a[0]-1]])
+[(0, 1, 0, 1, 1), (0, 0, 1, 1, 1), (1, 1, 1, 0, 0), (1, 0, 0, 1, 1), (1, 0, 1, 1, 0), (0, 1, 1, 0, 1), (1, 1, 0, 1, 0), (1, 0, 1, 0, 1), (1, 1, 0, 0, 1), (0, 1, 1, 1, 0)]
+
 
