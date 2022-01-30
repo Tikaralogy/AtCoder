8c92de75a33f141b24fb9cb6c4ae278ab55f4171
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2022-01-31 00:24:03 +0900

    2021/1/31

--- 練習/アルゴリズム実技検定3E.py
+++ 練習/アルゴリズム実技検定3E.py
@@ -1,5 +1,9 @@
 n, m, q = map(int,input().split())
-lsv = []
+lsuv = []
 for i in range(m):
-    lsv.append(list(map,input().split()))
-
+    lsuv.append(list(map,input().split()))
+lsc = list(map(int,input().split()))
+for i in range(q):
+    w = input()
+    if w[0]=="1":
+        print(lsc[int(w[3])])
