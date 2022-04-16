aa03755b75ec47ef271111126bef9d761e4e3b19
Tikaralogy <93646570+Tikaralogy@users.noreply.github.com>
2022-04-16 22:57:22 +0900

    2022/4/16

--- ガイドライン/ABC128C.py
+++ ガイドライン/ABC128C.py
@@ -4,5 +4,18 @@ for i in range(m):
     ls.append(list(map(int,input().split())))
 lsp = list(map(int, input().split()))
 
+x = 0
 for i in range(2**n):
-    
+    a = bin(i)[2:]
+    y = str(0)*(n-len(a))+a
+    a = 0
+    print(y)
+    for i in range(m):
+        z = 0
+        for a in ls[i][1:]:
+            z += int(y[a-1])
+        if z%2 == lsp[i]:
+            a += 1
+        if a == m:
+            x += 1
+print(x)
